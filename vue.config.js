17e5667c6e10304ba008f42c2a990128b145f6ec
zdx-1 <19546328912@163.com>
2024-04-17 14:48:10 +0800

    111

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port: 8080,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    },
+  },
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = 'Vue学习记录'
+      return args
+    })
+  }
 })
